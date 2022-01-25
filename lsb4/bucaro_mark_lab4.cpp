417d83dec8d5747a184a16fdecaa8e2c8a24ffb0
MJ-BUC <m.bucaro@outlook.com>
2022-01-24 20:01:29 -0600

    creation of lab 4 with some starter code

---
+++ lsb4/bucaro_mark_lab4.cpp
@@ -0,0 +1,24 @@
+// Mark Bucaro
+// CSCE 306 : Spring 2022
+// Lab4 - bucaro_mark_lab4.cpp
+// Code Summary: This source code file demonstrates the basic structure of a single page CPP application for lab1.
+
+#include <iostream>
+using namespace std;
+
+void print_array() {
+
+}
+
+double rturn_average() {
+
+}
+
+void reverse_and_print() {
+    
+}
+
+int main() {
+    std::cout << "My name is Mark!" << std::endl;
+    return 0;
+}
