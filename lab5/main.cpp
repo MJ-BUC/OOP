d7d70a71dbafa5bfdaf14c89143054d979714b4d
MJ-BUC <m.bucaro@outlook.com>
2022-02-14 08:19:36 -0600

    comments added to the fruit.h and main file

--- lab5/main.cpp
+++ lab5/main.cpp
@@ -14,9 +14,11 @@ using namespace std;
 int main() {
     cout << "Welcome to lab 5!" << endl << endl;
 
+    // instantiates 3 Fruit objects an apple, banana, and kiwi
     Fruit apple = Fruit("Apple", 100, 62, 0.19, 0.21, 14.8);
     Fruit banana = Fruit("Banana", 100, 85, 0.73, 0.22, 20.1);
     Fruit kiwi = Fruit("Kiwi", 100, 58, 1.06, 0.44, 14.00);
+    // prints the information each object holds
     apple.to_string();
     banana.to_string();
     kiwi.to_string();
