2388edf0699a23a4b3aa37c0bf2345b20331f8dc
MJ-BUC <m.bucaro@outlook.com>
2022-03-30 11:19:44 -0500

    tutoring problem involving pointers solved

--- lab9/Person.cpp
+++ lab9/Person.cpp
@@ -1,5 +1,38 @@
-//
-// Created by Mark on 3/30/2022.
-//
+// Mark Bucaro
+// CSCE 306: Spring 2022
+// Class Inheritance
+// Person will be the base class
+// file: Person.cpp
 
 #include "Person.h"
+#include "String"
+using namespace std;
+
+Person::Person(){}
+
+Person::Person(std::string a_fname, int an_age, std::string a_lname)
+        : firstname{a_fname}, personage{an_age}, lastname{a_lname} {}
+
+string Person::get_first(){
+    return firstname;
+}
+
+int Person::get_age(){
+    return personage;
+}
+
+string Person::get_last(){
+    return lastname;
+}
+
+void Person::set_first(int age){
+    this->firstname = age;
+}
+
+void Person::set_age(std::string fname){
+    this->personage = fname;
+}
+
+void Person::set_last(std::string lname){
+    this->lastname = lname;
+}
--- tutoring/main.cpp
+++ tutoring/main.cpp
@@ -2,8 +2,22 @@
 # include <iostream>
 using namespace std;
 
+void foo(int*** r, int len) {
+    for (int i = 0; i < len; i++)
+        *(r[i]) = *(r[len-1]);
+}
+
+
 int main() {
-    int** b = new int*[2];
-    *b[0] = 1;
+    int* a = new int(1);
+    int* b = new int(2);
+    int* c = new int(3);
+    int** d[] = {&a, &b, &c};
+
+    foo(d, 3);
+
+    for (int i = 0; i < 3; i++)
+        cout << **(d[i]) << endl;
+
     return 0;
 }
